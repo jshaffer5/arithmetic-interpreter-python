ae33322a5b31ea2c6df5e8c995a16ffbb2020c08
js <joshtwoone@gmail.com>
2020-10-04 23:48:51 -0700

    finished adding multiply operation

--- simple_interpreter_2.py
+++ simple_interpreter_2.py
@@ -1,7 +1,7 @@
 # Token types
 # EOF (end-of-file) token is used to indicate that
 # there is no more input left for lexical analysis
-INTEGER, PLUS, MINUS, EOF = 'INTEGER', 'PLUS', 'MINUS', 'EOF'
+INTEGER, PLUS, MINUS, MULTIPLY, EOF = 'INTEGER', 'PLUS', 'MINUS', 'MULTIPLY', 'EOF'
 
 
 class Token(object):
@@ -83,6 +83,10 @@ class Interpreter(object):
                 self.advance()
                 return Token(MINUS, '-')
 
+            if self.current_char == '*':
+                self.advance()
+                return Token(MULTIPLY, '*')
+
             self.error()
 
         return Token(EOF, None)
@@ -114,8 +118,10 @@ class Interpreter(object):
         op = self.current_token
         if op.type == PLUS:
             self.eat(PLUS)
-        else:
+        elif op.type == MINUS:
             self.eat(MINUS)
+        else:
+            self.eat(MULTIPLY)
 
         # we expect the current token to be an integer
         right = self.current_token
@@ -130,8 +136,10 @@ class Interpreter(object):
         # thus effectively interpreting client input
         if op.type == PLUS:
             result = left.value + right.value
-        else:
+        elif op.type == MINUS:
             result = left.value - right.value
+        else:
+            result = left.value * right.value
         return result
 
 
